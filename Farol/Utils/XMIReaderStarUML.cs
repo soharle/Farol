12b5c539908b985c954076f8ce6cdf94305fe96a
soharle <matheus7rod@gmail.com>
2019-05-06 23:43:06 -0300

    model-name + tests

--- Farol/Utils/XMIReaderStarUML.cs
+++ Farol/Utils/XMIReaderStarUML.cs
@@ -14,9 +14,12 @@ namespace Farol.Utils
             _model = new Model();
 
             XNamespace xmi = "http://schema.omg.org/spec/XMI/2.1";
+            XNamespace uml = "http://schema.omg.org/spec/UML/2.0";
             var classes = from item in doc.Descendants("packagedElement")
                           where item.Attribute(xmi + "type").Value == "uml:Class"
                           select item;
+            var modelNode = doc.Element(uml + "Model");
+            _model.Name = modelNode.Attribute("name").Value;
 
             foreach (var node in classes)
             {
---
+++ XmlReaderTest/StarUML/ReaderStarUML.cs
@@ -0,0 +1,55 @@
+﻿using Farol.Utils;
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using Xunit;
+
+namespace XmlReaderTest
+{
+    public class ReaderStarUML
+    {
+        public IXMIReader GetReaderStarUML_LoadedWith_1Classes()
+        {
+            var reader = new XMIReaderStarUML();
+            string workingDirectory = Path.GetFullPath(@"..\..\..\");
+            string file = "xmi/OnlyClassA.xmi";
+            string path = Path.Combine(workingDirectory + file);
+            XElement doc = XElement.Load($"{path}");
+            reader.LoadXmi(doc);
+            return reader;
+        }
+
+        [Fact]
+        public void MustRead_Name_Class_A()
+        {
+            var reader = GetReaderStarUML_LoadedWith_1Classes();
+            var model = reader.GetModel();
+            Assert.Equal("A", model.Classes[0].Name);
+        }
+
+        [Fact]
+        public void MustRead_Id_Class_A()
+        {
+            var reader = GetReaderStarUML_LoadedWith_1Classes();
+            var model = reader.GetModel();
+            Assert.Equal("AAAAAAFqfzSoEVy/JIk=", model.Classes[0].Id);
+        }
+
+        [Fact]
+        public void MustRead_Name_Model()
+        {
+            var reader = GetReaderStarUML_LoadedWith_1Classes();
+            var model = reader.GetModel();
+            Assert.Equal("RootModel", model.Name);
+        }
+
+        [Fact]
+        public void MustRead_Count_1_Classes()
+        {
+            var reader = GetReaderStarUML_LoadedWith_1Classes();
+            var model = reader.GetModel();
+            Assert.Single(model.Classes);
+        }
+    }
+}
