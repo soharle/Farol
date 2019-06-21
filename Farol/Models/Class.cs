66412dfa0abaf0f833da4bc7ca2c0d4886e6b202
soharle <matheus7rod@gmail.com>
2019-06-20 21:49:05 -0300

    OrderList added

---
+++ Farol/Models/Class.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Farol.Models
+{
+    public class Class
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<Class> Releases { get; set; }
+        public List<Class> ItDepends { get; set; }
+
+        public Class()
+        {
+            Releases = new List<Class>();
+            ItDepends = new List<Class>();
+        }
+
+        public int CompareTo(Class comparePart)
+        {
+
+            int difference = ItDepends.Count - comparePart.ItDepends.Count;
+
+            if(difference == 0)
+            {
+                return Releases.Count - comparePart.Releases.Count;
+            }
+            else
+            {
+                return difference;
+            }
+
+        }
+
+    }
+}
---
+++ Farol/Models/Step.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Farol.Models
+{
+    public class Step
+    {
+        public Class Class { get; set; }
+        public List<Class> Stubs { get; set; }
+        public List<Class> Releases { get; set; }
+
+    }
+}
